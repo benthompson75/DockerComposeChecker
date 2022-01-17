e312a420c113b2628250a809481ed85edf3d6feb
Ben Thompson <benthompson75@gmail.com>
2022-01-17 11:10:58 -0500

    Fixed loop error (indentation issue) Finished Static things Check

--- DockerComposeCheck.py
+++ DockerComposeCheck.py
@@ -4,22 +4,23 @@
 This script will scan docker-compose.yml files in a path and check for basic settings, as well as output common "nice to know" items
 
 TODO List - p = planned, i = in progress, c = completed, x = rejected
-  i Check for Static things
+  c Check for Static things
   . Check for Variable things
   . Output all Variable things
   . Enable command line switches to only output specific things
+  . Enable command line switch to check only specific file/dir
   . Create help output
   . Implement quiet/verbose output for static things
 
 """
 
 # Import classes
-#import os
-#import glob
-from pathlib import Path # to get list of files
-import re # for searching
-import sys # to open the files
-import fnmatch # to match filenames
+# import os
+# import glob
+from pathlib import Path  # to get list of files
+import re  # for searching
+import sys  # to open the files
+import fnmatch  # to match filenames
 
 # Static things to check for
 # PUID, GUID, TZ, UMASK, restart
@@ -37,47 +38,63 @@ UMASK_SET = "022"
 # Ignore "example" docker-compose files
 ignoreexample = True
 
-
-
 # Get list of docker-compose.yml files
 composefiles = []
 for file_path in Path(dockerdir).glob('**/docker-compose.yml'):
     if fnmatch.fnmatch(file_path, '*example*'):
-      if ignoreexample == True:
-        continue
+        if ignoreexample == True:
+            continue
     else:
-      composefiles.append(file_path)
-    # Start looping through and checking things
-    #### Right now this is looping multiple times and I don't know why yet.
-    #### I've confirmed that composefiles is only a single list of the files found
-    for cfile in composefiles:
-      # Re-init found vars for each loop
-      puidfound = False
-      guidfound = False
-      tzfound = False
-      umaskfound = False
-      print("Now inspecting ",cfile)
-      openfile = open(cfile, "r") # open the file for reading
-      # Check PUID
-      for line in openfile:
-        if re.search(PUID, line):
-          #print (line)
-          puidfound = True
-      if puidfound == False:
-        print ("No PUID defined!")
-  
-  
-  # Check GUID
-  
-  # Check TZ
-  
-  # Check UMASK - if not present, ignore
-  
+        composefiles.append(file_path)
+
+# Start looping through and checking things
+for cfile in composefiles:
+    # Re-init found vars for each loop
+    puidfound = False
+    pgidfound = False
+    tzfound = False
+    umaskfound = False
+    print("\nNow inspecting ", cfile)
+    openfile = open(cfile, "r")  # open the file for reading
+
+    # Check PUID
+    for puidline in openfile:
+        if re.search(PUID, puidline):
+            # print (line)
+            puidfound = True
+    if puidfound == False:
+        print("No PUID defined!")
+
+    # Check PGID
+    openfile.seek(0)  # reset to top of file
+    for pgidline in openfile:
+        if re.search(PGID, pgidline):
+            # print (line)
+            pgidfound = True
+    if pgidfound == False:
+        print("No PGID defined!")
+
+    # Check TZ
+    openfile.seek(0)  # reset to top of file
+    for tzline in openfile:
+        if re.search(TZ, tzline):
+            # print (line)
+            tzfound = True
+    if tzfound == False:
+        print("No TZ defined!")
+
+# Check UMASK - if not present, ignore
+    openfile.seek(0)  # reset to top of file
+    for umaskline in openfile:
+        if re.search(UMASK_SET, umaskline):
+            # print (line)
+            umaskfound = True
+    if umaskfound == False:
+        print("No UMASK_SET defined! (Optional)")
+
 
 # Port check
-#grep [[:digit:]]:[[:digit:]] ~/docker/*/docker-compose.yml
-#grep "^\s+\-" ~/docker/metube/docker-compose.yml
+# grep [[:digit:]]:[[:digit:]] ~/docker/*/docker-compose.yml
+# grep "^\s+\-" ~/docker/metube/docker-compose.yml
 
-#-T.
-
-
+# -T.
